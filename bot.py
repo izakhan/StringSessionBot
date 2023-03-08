38f571905ae7d4910abc4ba9fa8a1a6f1ccd4992
izakhan <121670888+izakhan@users.noreply.github.com>
2023-03-09 05:23:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import env
 import logging
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
-from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood
 
 logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
